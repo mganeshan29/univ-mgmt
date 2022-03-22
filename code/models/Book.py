451ff2c11f759ded35abad90f25ab44265f0c269
Gopaal Praturi <gopaalpraturi@gmail.com>
2022-03-22 14:35:28 +0530

    Add Book Course and Mark models

---
+++ code/models/Book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, id, edition, publisher, publishDate):
+        self.title = title
+        self.author = author
+        self.id = id
+        self.edition = edition
+        self.publisher = publisher
+        self.publishDate = publishDate
---
+++ code/models/Mark.py
@@ -0,0 +1,11 @@
+from tkinter import E
+
+
+class Mark:
+    def __init__(self, exam, role_no, marks, pass_marks, total):
+        self.exam = exam
+        self.role_no = role_no
+        self.marks = marks
+        self.pass_marks = pass_marks
+        self.total = total
+        
