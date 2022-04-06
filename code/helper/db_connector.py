e962c96aeaf935c6323d6ea92c8b75ed7c15f086
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2022-04-06 13:57:27 +0530

    Add gitignore

--- code/helper/db_connector.py
+++ code/helper/db_connector.py
@@ -5,22 +5,32 @@ class register_database:
         self.con1=mysql.connector.connect(host='localhost', user='admin', password='password',database='univmgmt')
         self.cursor1=self.con1.cursor()
 
-    def insert(self,query,values):
-        self.cursor1.execute(query,values)
+    def insert(self,query):
+        self.cursor1.execute(query)
         self.con1.commit()
+
     def select(self,query,values):
         self.cursor1.execute(query,values)
         row=self.cursor1.fetchone()
         self.con1.commit()
         return row
+
     def update(self,query,value):
         self.cursor1.execute(query,value)
         self.con1.commit()
+
     def delete(self,query,value):
         self.cursor1.execute(query,value)
         self.con1.commit()
+
     def select1(self,query):
         self.cursor1.execute(query)
         row = self.cursor1.fetchall()
         self.con1.commit()
         return row
+
+    # def inserRondi(self, query):
+    #     self.cursor1.execute('''
+    #     INSERT INTO AUTH VALUES("rondi2", "rondirondi");
+    #     ''')
+    #     self.con1.commit()
---
+++ code/helper/populate_db.py
@@ -0,0 +1,8 @@
+from . import register_database
+
+class populate_db:
+    TABLES = ['student', 'faculty', 'course', 'book', 'mark', 'library_dues', 'librarian', 'teacher', 'admin', 'fee_report', 'library']
+
+    def __init__(self):
+        self.conn = register_database()
+        
