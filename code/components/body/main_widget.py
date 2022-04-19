dab9e157206c7b4eb3e4b7471316a24301cb43e0
Ron Jacob <generalsmoke23@gmail.com>
2022-04-19 20:40:58 +0530

    add primary widget for tables

---
+++ code/components/body/main_widget.py
@@ -0,0 +1,127 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.font as font
+from PIL import Image, ImageTk
+
+from ..dashboard import dashboard
+from ..login_page import login_page as lp
+
+
+class mainWidget:
+    def __init__(self, title, master=None):
+        
+        # build ui
+        self.root = master
+        
+        logout_image = Image.open("code/images/LogOut.png")
+        logout_image = logout_image.resize((40, 40), Image.ANTIALIAS)
+        logout_image = ImageTk.PhotoImage(image=logout_image)
+        
+        previous_image = Image.open("code/images/previous.png")
+        previous_image = previous_image.resize((40, 40), Image.ANTIALIAS)
+        previous_image = ImageTk.PhotoImage(image=previous_image)
+        
+        
+        self.canvas1 = tk.Canvas(self.root)
+        self.canvas1.configure(background='white', height='600', relief='flat', width='800')
+        self.canvas1.pack(side='top')
+        
+        
+        self.header_font = font.Font(family="Raleway", size=13, weight="bold")
+        self.label_font = font.Font(family="Raleway", size=10, weight = "bold")
+        
+        page_header = ttk.Label(self.root)
+        page_header.configure(background='#ca0a4a')
+        page_header.place(anchor='nw', height='120', width='800', x='0', y='0')
+
+        
+        dash_layer1 = ttk.Label(self.root)
+        dash_layer1.configure(background='white')
+        dash_layer1.place(anchor='nw', height='80', relx='0.31',
+                          rely='0.04', width='270', x='0', y='0')
+        dash_layer2 = ttk.Label(self.root)
+        dash_layer2.configure(background='#ca0a4a')
+        dash_layer2.place(anchor='nw', height='70', relx='0.317',
+                          rely='0.05', width='260', x='0', y='0')
+        
+        dash_message = tk.Message(self.root)
+        dash_message.configure(background='white', foreground='#ca0a4a',font = self.header_font, text=title, width='200')
+        dash_message.place(anchor='nw', height='63', relx='0.322',
+                           rely='0.057', width='253', x='0', y='0')
+        
+        x='0';y='0.2'
+        
+        if(title != "Fee Report"):
+            fee_button = tk.Button(self.root, command = self.onOpenFeeReport)
+            fee_button.configure(text='Fee Report', width='11',font = self.label_font, activebackground = "#cf235c", activeforeground = "white", fg = "white", bg = "#ca0a4a",bd = '2', highlightthickness= 0)
+            fee_button.place(anchor='nw', height='45', rely='0.2', x='0', y='0')
+            
+        if(title != "Marksheet") :
+            mark_button = tk.Button(self.root, command = self.onOpenMarks)
+            mark_button.configure(text='Marksheet', width='11',font = self.label_font, activebackground = "#cf235c", activeforeground = "white", fg = "white",  bg = "#ca0a4a",bd = '2', highlightthickness= 0)
+            mark_button.place(anchor='nw', height='45', relx='0.13', rely='0.2', x='0', y='0')
+        else:
+            x='0.13';y='0.2'
+            
+            
+        if(title != "Library"):
+            lib_button = tk.Button(self.root, command = self.onOpenLibrary)
+            lib_button.configure(text='Library', width='11',font = self.label_font, activebackground = "#cf235c", activeforeground = "white", fg = "white",  bg = "#ca0a4a",bd = '2', highlightthickness= 0)
+            lib_button.place(anchor='nw', height='45', relx='0.26', rely='0.2', x='0', y='0')
+        else:
+            x='0.26';y='0.2'
+            
+            
+        if(title != "Profile"):
+            profile_button = tk.Button(self.root, command = self.onOpenProfile)
+            profile_button.configure(text='Profile', width='11',font = self.label_font, activebackground = "#cf235c", activeforeground = "white", fg = "white", bg = "#ca0a4a",bd = '2', highlightthickness= 0)
+            profile_button.place(anchor='nw', height='45', relx='0.38', rely='0.2', x='0', y='0')
+        else:
+            x='0.38';y='0.2'
+            
+        active_button = tk.Button(self.root)
+        active_button.configure(text=title, width='11',font = self.label_font, activebackground = "#d38a92", activeforeground ="#ca0a4a", bg = "#ea99a2", fg = "#ca0a4a",bd = '2', highlightthickness= 0)
+        active_button.place(anchor='nw', height='45', relx=x, rely=y, x='0', y='0')
+        
+        logout_button = tk.Button(self.root,command = self.logout, bd = 0,highlightthickness=0,activebackground="#ca0a4a",bg="#ca0a4a", image=logout_image)
+        logout_button.image = logout_image
+        logout_button.place(anchor="nw", relx="0.9",
+                            rely="0.055", x="0", y='0')
+        
+        previous_button = tk.Button(self.root,command = self.onOpenDashboard, bd = 0,highlightthickness=0,activebackground="#ca0a4a",bg="#ca0a4a", image=previous_image)
+        previous_button.image = previous_image
+        previous_button.place(anchor="nw", relx="0.04",
+                            rely="0.055", x="0", y='0')
+        
+        self.root.configure(height='200', width='200')
+        
+        
+    def logout(self):
+        self.root.destroy()
+        lp.LoginPage("Student")
+        
+        
+    def onOpenProfile(self):
+        self.canvas1.destroy()
+        mainWidget("Profile", self.root)    
+        
+    def onOpenLibrary(self):
+        self.canvas1.destroy()
+        mainWidget("Library", self.root)
+        
+    def onOpenFeeReport(self):
+        self.canvas1.destroy()
+        mainWidget("Fee Report", self.root)
+    
+    def onOpenMarks(self):
+        self.canvas1.destroy()
+        mainWidget("Marksheet",self.root)
+            
+    def onOpenDashboard(self):
+        self.root.destroy()
+        dashboard.DashBoard()
+        
+        
+
+
+
--- code/components/dashboard/dashboard.py
+++ code/components/dashboard/dashboard.py
@@ -6,17 +6,20 @@ from PIL import Image, ImageTk
 from ...helper import constants as const
 from ..login_page import login_page as LP
 
+from ..body import main_widget as main
+
 class DashBoard:
     def __init__(self, title='', master=None, role = const.STUDENT_ROLE):
         if (role == const.STUDENT_ROLE):
+           
             self.title = title
             # build ui
             self.root = tk.Tk() if master is None else tk.Toplevel(master)
             self.root.title("University Management System")
 
-            canvas1 = tk.Canvas(self.root)
-            canvas1.configure(background='white', height='600', width='800')
-            canvas1.pack(side='top')
+            self.canvas1 = tk.Canvas(self.root)
+            self.canvas1.configure(background='white', height='600', width='800')
+            self.canvas1.pack(side='top')
 
             self.header_font = font.Font(family="Raleway", size=13, weight="bold")
             self.button_font = font.Font(family="Raleway", size=10, weight="bold")
@@ -59,22 +62,21 @@ class DashBoard:
             dash_message.place(anchor='nw', height='80', relx='0.323',
                             rely='0.10', width='280', x='0', y='0')
 
-            mark_button = tk.Button(self.root, bg="white", image=mark_image)
+            mark_button = tk.Button(self.root, bg="white", image=mark_image,command = self.onOpenMarks)
             mark_button.image = mark_image
             mark_button.place(anchor='nw', height='188', relx='0.05',
                             rely='0.27', width='188', x='0', y='0')
 
-            library_button = tk.Button(self.root, bg="white", image=lib_image)
+            library_button = tk.Button(self.root, bg="white", image=lib_image,command = self.onOpenLibrary)
             library_button.image = lib_image
             library_button.place(anchor='nw', height='188',
                                 relx='0.38', rely='0.27', width='188', x='0', y='0')
-
-            fee_button = tk.Button(self.root, bg="white", image=fee_image)
+            fee_button = tk.Button(self.root, bg="white", image=fee_image, command = self.onOpenFeeReport)
             fee_button.image = fee_image
             fee_button.place(anchor='nw', height='188', relx='0.7',
                             rely='0.27', width='188', x='0', y='0')
             
-            profile_button = tk.Button(self.root, bg="white", image=profile_image)
+            profile_button = tk.Button(self.root, bg="white", image=profile_image, command = self.onOpenProfile)
             profile_button.image = profile_image
             profile_button.place(anchor='nw', height='188', relx='0.05',
                             rely='0.65', width='188', x='0', y='0')
@@ -83,10 +85,32 @@ class DashBoard:
             logout_button.image = logOut_image
             logout_button.place(anchor='nw', height='188', relx='0.38',
                             rely='0.65', width='188', x='0', y='0')
+            
 
             self.root.configure(height='200', width='200')
+
             self.root.mainloop()
 
     def logout(self):
         self.root.destroy()
-        LP.LoginPage()
+        lp.LoginPage("Student")
+        
+        
+    def onOpenProfile(self):
+        self.canvas1.destroy()
+        main.mainWidget("Profile", self.root)    
+        
+    def onOpenLibrary(self):
+        self.canvas1.destroy()
+        main.mainWidget("Library", self.root)
+        
+    def onOpenFeeReport(self):
+        self.canvas1.destroy()
+        main.mainWidget("Fee Report", self.root)
+    
+    def onOpenMarks(self):
+        self.canvas1.destroy()
+        main.mainWidget("Marksheet",self.root)
+            
+        
+        
